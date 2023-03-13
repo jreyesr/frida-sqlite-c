83a78e0a5f15075b54a69b3fbee6c57f33a7504d
jreyesr <jreyesr@users.noreply.github.com>
2023-03-12 22:22:02 -0500

    Intercept bind and exec calls

--- captain_hook.py
+++ captain_hook.py
@@ -4,15 +4,20 @@ import sys
 def on_message(message, data):
     print("[on_message] message:", message, "data:", data)
 
-pid = frida.spawn("main", ["main", "test.sqlite3", "nonexistent"]) # Start the program, but paused
+pid = frida.spawn("main", ["main", "test.sqlite3", "bob"]) # Start the program, but paused
 session = frida.attach(pid) # Hook into the program
 
 script = session.create_script("""
 Interceptor.attach(Module.getExportByName("libsqlite3.so.0.8.6", "sqlite3_prepare_v2"), {
-    onEnter(args) {
-        send(args[1].readCString());
-    }
+    onEnter(args) { send(args[1].readCString()); }
 });
+Interceptor.attach(Module.getExportByName("libsqlite3.so.0.8.6", "sqlite3_bind_text"), {
+    onEnter(args) { send(args[2].readCString()); }
+});
+Interceptor.attach(Module.getExportByName("libsqlite3.so.0.8.6", "sqlite3_step"), {
+    onEnter(args) { send("RUNNING STATEMENT!"); }
+});
+
 """)
 script.on("message", on_message)
 script.load()
