a4e40aa0b0182909802f6ce80ab17fb7501d7020
jreyesr <jreyesr@users.noreply.github.com>
2023-03-12 22:00:10 -0500

    Read the SQL statements made by the app

--- captain_hook.py
+++ captain_hook.py
@@ -1,19 +1,22 @@
 import frida
+import sys
 
 def on_message(message, data):
     print("[on_message] message:", message, "data:", data)
 
-pid = frida.spawn("main", ["main", "test.sqlite3", "bob"]) # Start the program, but paused
+pid = frida.spawn("main", ["main", "test.sqlite3", "nonexistent"]) # Start the program, but paused
 session = frida.attach(pid) # Hook into the program
 
 script = session.create_script("""
-rpc.exports.enumerateModules = () => {
-  return Module.getExportByName("libsqlite3.so.0.8.6", "sqlite3_prepare_v2");
-};
+Interceptor.attach(Module.getExportByName("libsqlite3.so.0.8.6", "sqlite3_prepare_v2"), {
+    onEnter(args) {
+        send(args[1].readCString());
+    }
+});
 """)
 script.on("message", on_message)
 script.load()
 
-print(script.exports_sync.enumerate_modules())
-
 frida.resume(pid) # Run the program
+
+sys.stdin.read()  # IMPORTANT: Otherwise the script terminates before the messages are received
