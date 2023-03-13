c3c4755d49011f652edd668f58cc556d386dc336
jreyesr <jreyesr@users.noreply.github.com>
2023-03-12 21:40:34 -0500

    Get the address of a function

--- captain_hook.py
+++ captain_hook.py
@@ -8,12 +8,12 @@ session = frida.attach(pid) # Hook into the program
 
 script = session.create_script("""
 rpc.exports.enumerateModules = () => {
-  return Process.enumerateModules();
+  return Module.getExportByName("libsqlite3.so.0.8.6", "sqlite3_prepare_v2");
 };
 """)
 script.on("message", on_message)
 script.load()
 
-print([m["name"] for m in script.exports_sync.enumerate_modules()])
+print(script.exports_sync.enumerate_modules())
 
 frida.resume(pid) # Run the program
